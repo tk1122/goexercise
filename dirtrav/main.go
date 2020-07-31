b73ddd4a03aaf824fcbb73f8e790b86c1f4530be
Ngô Trường Sinh <sinhnt@bap.jp>
2020-07-31 21:57:49 +0700

    directory traverse with cancellation

--- dirtrav/main.go
+++ dirtrav/main.go
@@ -8,7 +8,9 @@ import (
 	"sync"
 	"time"
 )
+
 var gaurd = make(chan struct{}, 30)
+var done = make(chan struct{})
 
 func main() {
 	start := time.Now()
@@ -16,7 +18,7 @@ func main() {
 	var wg sync.WaitGroup
 
 	// span worker
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 3; i++ {
 		wg.Add(1)
 		go walkDirs("/usr", fileSizes, &wg)
 	}
@@ -27,12 +29,23 @@ func main() {
 		close(fileSizes)
 	}()
 
+	//interupter
+	go func() {
+		_, _ = os.Stdin.Read(make([]byte, 1))
+		close(done)
+	}()
+
 	numFile, totalFileSize := 0, 0
 	tick := time.NewTicker(500 * time.Millisecond)
 
 loop:
 	for {
 		select {
+		case <-done:
+			// drain channel to make goroutines exist
+			for range fileSizes {
+			}
+			return
 		case fs, ok := <-fileSizes:
 			if !ok {
 				tick.Stop()
@@ -49,17 +62,28 @@ loop:
 	fmt.Println(numFile)
 	fmt.Println(totalFileSize)
 	fmt.Println(time.Since(start))
+	panic("")
 }
 
 func walkDirs(dir string, fileSizes chan int64, wg *sync.WaitGroup) {
 	defer wg.Done()
+	if cancelled() {
+		return
+	}
 	walkDirsRecursive(dir, fileSizes)
 }
 
 func dirents(dir string) []os.FileInfo {
-	gaurd <- struct{}{}
+	select {
+	case gaurd <- struct{}{}:
+	case <-done:
+		return nil
+	}
+
+	defer func() {
+		<-gaurd
+	}()
 	entries, err := ioutil.ReadDir(dir)
-	<- gaurd
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "reading directory %s failed: %s\n", dir, err)
 		return nil
@@ -68,6 +92,9 @@ func dirents(dir string) []os.FileInfo {
 }
 
 func walkDirsRecursive(dir string, fileSizes chan<- int64) {
+	if cancelled() {
+		return
+	}
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
 			subdir := filepath.Join(dir, entry.Name())
@@ -75,5 +102,14 @@ func walkDirsRecursive(dir string, fileSizes chan<- int64) {
 		} else {
 			fileSizes <- entry.Size()
 		}
+	}
+}
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
 	}
 }
